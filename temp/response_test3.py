e6a6a037a97fe28c1f22c6c34b05375db8973133
Kozaka <kozaka.gamer@gmail.com>
2022-03-03 01:21:06 +0100

    #83: unused temp directory ignored.

--- temp/response_test3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import json
-from urllib.request import urlopen
-
-with urlopen('http://192.168.178.48:9938/') as response:
-    result = json.loads(response.read().decode(response.headers.get_content_charset('utf-8')))
-
-print(result['songDetails']['songName'])
