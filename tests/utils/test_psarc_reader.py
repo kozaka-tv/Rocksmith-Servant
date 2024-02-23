c00a3ad087747289028152adfec34662d98b8272
ZsannaK <zhannakuprienko@gmail.com>
2024-02-23 01:38:56 +0100

    revert first commit tryout

--- tests/utils/test_psarc_reader.py
+++ tests/utils/test_psarc_reader.py
@@ -4,7 +4,6 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
-        # TODO just to test my first commit
 
 
 if __name__ == '__main__':
