0e5a897876c56a67ba04794808f224a8ab4102a5
ZsannaK <zhannakuprienko@gmail.com>
2024-02-23 01:38:28 +0100

    my first commit

--- tests/utils/test_psarc_reader.py
+++ tests/utils/test_psarc_reader.py
@@ -4,6 +4,7 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
+        # TODO just to test my first commit
 
 
 if __name__ == '__main__':
