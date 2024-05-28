2f0dbaa4a2bad8d08f1669c7d3db0477284b0c4f
Kozaka <kozaka.gamer@gmail.com>
2024-05-28 16:24:44 +0200

    #213 tryout: os.path.dirname

--- run.py
+++ run.py
@@ -33,9 +33,9 @@ log.warning("-------------------------------------------------------------------
 
 # TODO remove this later!
 log.warning('program_location=%s', os.path.abspath(sys.argv[0]))
-log.warning('call_location=%s',  os.getcwd())
-log.warning('containing_directory=%s',   os.path.dirname(os.path.abspath(sys.argv[0])))
-
+log.warning('call_location=%s', os.getcwd())
+log.warning('containing_directory=%s', os.path.dirname(os.path.abspath(sys.argv[0])))
+log.warning('os.path.dirname(__file__)=%s', os.path.dirname(__file__))
 
 HEARTBEAT = 1
 HEARTBEAT_MANAGE_SONGS = 1
