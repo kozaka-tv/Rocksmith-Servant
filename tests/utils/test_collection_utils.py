8deba3ee916b1b7d1025c48b2b8f5669e610d4bd
Kozaka <kozaka.gamer@gmail.com>
2024-06-02 18:49:53 +0200

    #220 collection utils test fixed for Unix

--- tests/utils/test_collection_utils.py
+++ tests/utils/test_collection_utils.py
@@ -1,6 +1,10 @@
+import os
+
 import pytest
 
 from utils import collection_utils
+
+NL = os.linesep
 
 
 @pytest.mark.parametrize(
@@ -95,22 +99,22 @@ def test_set_of_the_tuples_from_the_first_position__with_2_tuples_and_int_in_the
         (set(), ''),
         (dict(), ''),
 
-        (['line1'], '\r\nline1'),
-        (['line1', 'line2'], '\r\nline1\r\nline2'),
-        (['line1', 'line2', 'line3'], '\r\nline1\r\nline2\r\nline3'),
+        (['line1'], NL + 'line1'),
+        (['line1', 'line2'], NL + 'line1' + NL + 'line2'),
+        (['line1', 'line2', 'line3'], NL + 'line1' + NL + 'line2' + NL + 'line3'),
 
-        ({
-             'key1': {'key1-item1': 'key1-item1-value1', 'key1-item2': 'key1-item2-value1'}
-         },
-         ('\r\n'
-          "key1: ({'key1-item1': 'key1-item1-value1', 'key1-item2': 'key1-item2-value1'})")),
-        ({
-             'key1': {'key1-item1': 'key1-item1-value1', 'key1-item2': 'key1-item2-value1'},
-             'key2': {'key2-item1': 'key2-item1-value1', 'key2-item2': 'key2-item2-value1'},
-         },
-         ('\r\n'
-          "key1: ({'key1-item1': 'key1-item1-value1', 'key1-item2': 'key1-item2-value1'})\r\n"
-          "key2: ({'key2-item1': 'key2-item1-value1', 'key2-item2': 'key2-item2-value1'})"))
+        (
+                {'key1': {'key1-item1': 'key1-item1-value1', 'key1-item2': 'key1-item2-value1'}},
+                NL + "key1: ({'key1-item1': 'key1-item1-value1', 'key1-item2': 'key1-item2-value1'})"
+        ),
+        (
+                {
+                    'key1': {'key1-item1': 'key1-item1-value1', 'key1-item2': 'key1-item2-value1'},
+                    'key2': {'key2-item1': 'key2-item1-value1', 'key2-item2': 'key2-item2-value1'},
+                },
+                NL + "key1: ({'key1-item1': 'key1-item1-value1', 'key1-item2': 'key1-item2-value1'})" +
+                NL + "key2: ({'key2-item1': 'key2-item1-value1', 'key2-item2': 'key2-item2-value1'})"
+        )
     ]
 )
 def test_repr_in_multi_line(test_input, expected):
