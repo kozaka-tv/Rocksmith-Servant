a2123cef8475bb84df79c68efaafcbc3dcbab797
ZsannaK <zhannakuprienko@gmail.com>
2024-06-03 23:42:16 +0200

    #258 pylint fixes

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -59,3 +59,7 @@ class BadDirectoryError(Exception):
 class ExtractError(Exception):
     def __init__(self, error_msg):
         super().__init__(error_msg)
+
+class ExtractError(Exception):
+    def __init__(self, error_msg):
+        super().__init__(error_msg)
