4d2e10a8ed425eccb92c5edff7dabcfdb22b94d7
Kozaka <kozaka.gamer@gmail.com>
2024-07-01 14:52:19 +0200

    #136 - normalized artist and title saved also in the DB song table - refactoring: SongData has now an ArtistTitle subclass. - normalize is now also puts everything to lowercase

--- modules/database/db_manager.py
+++ modules/database/db_manager.py
@@ -1,11 +1,11 @@
 import logging
 import os
 import sqlite3
+from typing import Optional
 
-from modules.song_loader.song_data import SongData
-from utils import string_utils
+from modules.song_loader.song_data import SongData, ArtistTitle
 from utils.collection_utils import get_tuples_from_the_first_position_of
-from utils.string_utils import remove_special_chars
+from utils.string_utils import normalize, escape_single_quote
 
 log = logging.getLogger()
 
@@ -47,11 +47,11 @@ class DBManager:
         return get_tuples_from_the_first_position_of(songs)
 
     def search_song_by_artist_and_title(self, artist, title):
-        songs_from_db_without_special_chars = self.__get_songs_from_db(remove_special_chars(artist),
-                                                                       remove_special_chars(title))
+        songs_from_db_without_special_chars = self.__get_songs_from_db(normalize(artist),
+                                                                       normalize(title))
         return self.__get_songs_from_db(artist, title).union(songs_from_db_without_special_chars)
 
-    def search_song_by_filename(self, filename: str):
+    def search_song_by_filename(self, filename: str) -> Optional[SongData]:
         cur = self.db.cursor()
         song = cur.execute("SELECT colFileName, colArtist, colTitle FROM songs where songs.colFileName = ?",
                            ("" + filename,)).fetchone()
@@ -59,12 +59,7 @@ class DBManager:
         if song is None:
             return None
 
-        song_data = SongData()
-        song_data.song_filename = song[0]
-        song_data.artist = song[1]
-        song_data.title = song[2]
-
-        return song_data
+        return SongData(song_filename=(song[0]), artist_title=ArtistTitle(song[1], song[2]))
 
     def is_song_by_filename_exists(self, filename):
         cur = self.db.cursor()
@@ -72,16 +67,24 @@ class DBManager:
         fetchone = execute.fetchone()[0]
         return fetchone != 0
 
-    def insert_song(self, song_data):
+    def insert_song(self, song_data: SongData):
         cur = self.db.cursor()
-        sql = (("insert into songs ("
-                "colArtist, "
-                "colTitle, "
-                "colFileName) " +
-                "values ('")
-               + string_utils.escape_single_quote(song_data.artist) + "', '"
-               + string_utils.escape_single_quote(song_data.title) + "', '"
-               + string_utils.escape_single_quote(song_data.song_filename) + "')")
+        sql = (
+                ("insert into songs ("
+                 "colArtist, "
+                 "colTitle, "
+                 "artistNormalized, "
+                 "titleNormalized, "
+                 "colFileName"
+                 ") " +
+                 "values ('")
+                + escape_single_quote(song_data.artist_title.artist) + "', '"
+                + escape_single_quote(song_data.artist_title.title) + "', '"
+                + escape_single_quote(song_data.artist_title.artist_normalized()) + "', '"
+                + escape_single_quote(song_data.artist_title.title_normalized()) + "', '"
+                + escape_single_quote(song_data.song_filename)
+                + "')"
+        )
         cur.execute(sql)
         self.db.commit()
 
